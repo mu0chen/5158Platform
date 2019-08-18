d54543c7b45dc60c152288f0370c52ea175213b3
monster <469408434@qq.com>
2019-08-18 22:17:55 +0800

    构建maven工程、项目结构

---
+++ src/main/java/com/platform/web/PlatformWebStart.java
@@ -0,0 +1,18 @@
+package com.platform.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClaseName MonsterWebStart
+ * @Description
+ * @Author Monster
+ * @Date 2019/8/8 13:03
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class PlatformWebStart {
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformWebStart.class, args);
+    }
+}
